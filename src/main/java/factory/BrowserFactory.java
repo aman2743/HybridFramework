b00f16246471db71d79cc6523f64b3f4f4d9757a
Aman Kumar <Kush@274306>
2026-01-28 14:04:29 +0530

    commit

---
+++ src/main/java/factory/BrowserFactory.java
@@ -0,0 +1,33 @@
+package factory;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class BrowserFactory {
+	private BrowserFactory() { //No one can create object of this BrowserFactory class
+//		No logic require here
+	}
+	public static WebDriver createInstance(String browserName) {
+		WebDriver driver=null;
+		switch (browserName.toLowerCase()) {
+		case "chrome":
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+			break;
+		case "edge":
+			WebDriverManager.edgedriver().setup();
+			driver = new EdgeDriver();
+			break;
+		case "firefox":
+			WebDriverManager.firefoxdriver().setup();
+			driver = new FirefoxDriver();
+			break;
+		default:
+			System.out.println("incorrect browser info");
+		}
+		return driver;
+	}
+}
